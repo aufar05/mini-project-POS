e3cacb8c2aa085ae84a8d87f9ee32ea33e6bb7bc
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-23 14:40:50 +0700

    tambah layout dasar

---
+++ point-of-sales/src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-grow">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
