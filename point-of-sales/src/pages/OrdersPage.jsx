b567f9a597b2e9be212ed6a3e365b938939823bd
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-23 14:41:42 +0700

    tambah halaman order

---
+++ point-of-sales/src/pages/OrdersPage.jsx
@@ -0,0 +1,12 @@
+import Orders from "../components/Orders";
+import Layout from "../layout/Layout";
+
+const OrdersPage = () => {
+  return (
+    <Layout>
+      <Orders></Orders>
+    </Layout>
+  );
+};
+
+export default OrdersPage;
