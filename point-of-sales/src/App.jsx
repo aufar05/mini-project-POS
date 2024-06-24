bf9dbbefd650a91be059530f0095ade5d9a01fdc
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-24 23:05:58 +0700

    tambah router

--- point-of-sales/src/App.jsx
+++ point-of-sales/src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import OrdersPage from "./pages/OrdersPage.jsx";
+import PaymentPage from "./pages/PaymentPage.jsx";
 
 function App() {
-  return <OrdersPage></OrdersPage>;
+  return (
+    <Routes>
+      <Route path="/" element={<OrdersPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+    </Routes>
+  );
 }
-
 export default App;
