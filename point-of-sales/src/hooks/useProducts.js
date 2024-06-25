379a0edb3ab35ef3b4f3c023d4543d8752e32765
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-25 20:38:15 +0700

    update syntax

--- point-of-sales/src/hooks/useProducts.js
+++ point-of-sales/src/hooks/useProducts.js
@@ -6,7 +6,7 @@ const fetcher = async (url) => {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error:", error);
     throw error;
   }
 };
