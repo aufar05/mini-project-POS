d1cf4cb539af6a982ac74f8928cd7748075490b0
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-25 20:33:56 +0700

    menambah rute bagian riwayat transaksi

--- point-of-sales/src/App.jsx
+++ point-of-sales/src/App.jsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import OrdersPage from "./pages/OrdersPage.jsx";
 import PaymentPage from "./pages/PaymentPage.jsx";
+import HistoryPage from "./pages/HistoryPage.jsx";
+import HistoryDetailPage from "./pages/HistoryDetailPage.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<OrdersPage />} />
       <Route path="/payment" element={<PaymentPage />} />
+      <Route path="/history" element={<HistoryPage />} />
+      <Route
+        path="/history/detail-transaksi/:id"
+        element={<HistoryDetailPage />}
+      />
     </Routes>
   );
 }
