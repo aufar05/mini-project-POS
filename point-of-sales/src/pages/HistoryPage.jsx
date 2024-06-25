c864a12d21f9dacb5ed4be3badb0064101cae1aa
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-25 20:39:16 +0700

    tambah halaman riwayat transaksi

---
+++ point-of-sales/src/pages/HistoryPage.jsx
@@ -0,0 +1,12 @@
+import History from "../components/History";
+import Layout from "../layout/Layout";
+
+const HistoryPage = () => {
+  return (
+    <Layout>
+      <History />
+    </Layout>
+  );
+};
+
+export default HistoryPage;
