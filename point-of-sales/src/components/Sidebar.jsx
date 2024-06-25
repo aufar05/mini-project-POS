0ae0858e31c84c581a2ce01a8b95cb0781e5db4a
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-25 20:36:05 +0700

    update sidebar terintegrasi dengan route

--- point-of-sales/src/components/Sidebar.jsx
+++ point-of-sales/src/components/Sidebar.jsx
@@ -1,15 +1,41 @@
+import { NavLink } from "react-router-dom";
+import { FaClipboardList, FaHistory } from "react-icons/fa";
+import { MdProductionQuantityLimits, MdCategory } from "react-icons/md";
+
+const getClassName = ({ isActive }) =>
+  `p-4 block cursor-pointer  ${
+    isActive
+      ? "bg-gray-700 border-l-[0.15rem] border-white  flex items-center    "
+      : "hover:bg-gray-700 flex items-center"
+  }`;
+
 const Sidebar = () => {
   return (
-    // menambah flex shrink agar ukuran sidebar relatif sama
-    <div className="h-screen w-56 bg-gray-800 text-white flex-shrink-0">
-      <div className="p-4 text-2xl font-bold border-b border-gray-700">
+    <div className="h-screen w-56 bg-gray-800  text-white flex-shrink-0">
+      <div className="p-4 text-2xl font-bold border-b  border-gray-700  ">
         FinalProjectPOS
       </div>
       <ul className="mt-4">
-        <li className="p-4 hover:bg-gray-700 cursor-pointer">Order</li>
-        <li className="p-4 hover:bg-gray-700 cursor-pointer">Riwayat</li>
-        <li className="p-4 hover:bg-gray-700 cursor-pointer">Produk</li>
-        <li className="p-4 hover:bg-gray-700 cursor-pointer">Kategori</li>
+        <li>
+          <NavLink to="/" className={getClassName}>
+            <FaClipboardList className="mr-2" /> Order
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/history" className={getClassName}>
+            <FaHistory className="mr-2" /> Riwayat
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/produk" className={getClassName}>
+            <MdProductionQuantityLimits className="mr-2" /> Produk
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/kategori" className={getClassName}>
+            <MdCategory className="mr-2" /> Kategori
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
