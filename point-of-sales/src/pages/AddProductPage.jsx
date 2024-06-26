aece0576e8f4ed1c2b35990e5e4d5a004ecaa411
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-26 10:36:10 +0700

    tambah halaman add produk

---
+++ point-of-sales/src/pages/AddProductPage.jsx
@@ -0,0 +1,12 @@
+import AddProductForm from "../components/AddProduct";
+import Layout from "../layout/Layout";
+
+const AddProductPage = () => {
+  return (
+    <Layout>
+      <AddProductForm />
+    </Layout>
+  );
+};
+
+export default AddProductPage;
