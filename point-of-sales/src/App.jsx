73130fce4e85a32769eccadf15b87be906f7fe1b
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-26 10:32:38 +0700

    tambah route produk , dan tambah produk

--- point-of-sales/src/App.jsx
+++ point-of-sales/src/App.jsx
@@ -3,6 +3,8 @@ import OrdersPage from "./pages/OrdersPage.jsx";
 import PaymentPage from "./pages/PaymentPage.jsx";
 import HistoryPage from "./pages/HistoryPage.jsx";
 import HistoryDetailPage from "./pages/HistoryDetailPage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+import AddProductPage from "./pages/AddProductPage.jsx";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         path="/history/detail-transaksi/:id"
         element={<HistoryDetailPage />}
       />
+      <Route path="/produk" element={<ProductPage />} />
+      <Route path="/produk/tambah-produk" element={<AddProductPage />} />
     </Routes>
   );
 }
