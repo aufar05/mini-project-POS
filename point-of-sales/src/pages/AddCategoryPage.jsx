34ebfb66bdabffdedecfe322b47e5105d59cfa5a
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-28 08:24:35 +0700

    halaman tambah kategori

---
+++ point-of-sales/src/pages/AddCategoryPage.jsx
@@ -0,0 +1,12 @@
+import AddCategoryForm from "../components/AddCategory";
+import Layout from "../layout/Layout";
+
+const AddCategoryPage = () => {
+  return (
+    <Layout>
+      <AddCategoryForm />
+    </Layout>
+  );
+};
+
+export default AddCategoryPage;
