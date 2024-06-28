e8def1b28a2063e39916161b9577886a2005ddb0
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-28 08:25:10 +0700

    halaman kategori

---
+++ point-of-sales/src/pages/CategoryPage.jsx
@@ -0,0 +1,12 @@
+import Category from "../components/Category";
+import Layout from "../layout/Layout";
+
+const CategoryPage = () => {
+  return (
+    <Layout>
+      <Category />
+    </Layout>
+  );
+};
+
+export default CategoryPage;
