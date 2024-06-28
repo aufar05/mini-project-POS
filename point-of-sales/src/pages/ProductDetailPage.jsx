0f20c12537c4b4cecde8b79d560ffcbb7bb2f850
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-28 08:25:23 +0700

    halaman produk detail

---
+++ point-of-sales/src/pages/ProductDetailPage.jsx
@@ -0,0 +1,12 @@
+import ProductDetail from "../components/ProductDetail";
+import Layout from "../layout/Layout";
+
+const ProductDetailPage = () => {
+  return (
+    <Layout>
+      <ProductDetail />
+    </Layout>
+  );
+};
+
+export default ProductDetailPage;
