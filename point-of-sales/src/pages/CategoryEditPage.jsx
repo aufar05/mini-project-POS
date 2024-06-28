1e64c31355f2a6660c3ef10c81ae3f4d921d0d6b
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-28 08:24:59 +0700

    halaman edit kategori

---
+++ point-of-sales/src/pages/CategoryEditPage.jsx
@@ -0,0 +1,12 @@
+import EditCategoryForm from "../components/EditCategory";
+import Layout from "../layout/Layout";
+
+const CategoryEditPage = () => {
+  return (
+    <Layout>
+      <EditCategoryForm />
+    </Layout>
+  );
+};
+
+export default CategoryEditPage;
