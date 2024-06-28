034662d3ecd10f7a858780ff7e5c5c48b375bfcb
Idzan Aufar <aufar.idzan@gmail.com>
2024-06-28 08:25:33 +0700

    halaman edit produk

---
+++ point-of-sales/src/pages/ProductEditPage.jsx
@@ -0,0 +1,12 @@
+import EditProductForm from "../components/EditProduct";
+import Layout from "../layout/Layout";
+
+const ProductEditPage = () => {
+  return (
+    <Layout>
+      <EditProductForm />
+    </Layout>
+  );
+};
+
+export default ProductEditPage;
