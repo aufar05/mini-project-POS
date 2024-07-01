906671cdb6e6aa8fa4fb027ecad371d83cb8ebad
Idzan Aufar <aufar.idzan@gmail.com>
2024-07-01 17:24:13 +0700

    komponen loading

---
+++ point-of-sales/src/utils/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-dashed border-gray-400 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
